e5629992c2bf02249572660be1cf278d26f4e334
JoacoVillegas <cocoproman11@gmail.com>
2024-09-17 10:35:40 -0300

    Logica Two Players

--- club_proyect/mage_1.gd
+++ club_proyect/mage_1.gd
@@ -1,36 +1,33 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $AnimatedSprite2D
+
+@export_range(1,2) var jugador : int
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
 
-@onready var animated_sprite = $AnimatedSprite2D
 var is_teleporting = false  # Nueva variable para controlar si el mago está haciendo "destello"
 func get_input():
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	velocity = input_dir * SPEED
+	var input_dir
+	if jugador == 1:
+		input_dir = Input.get_vector("izquierda", "derecha", "arriba", "abajo")
 	
+	if jugador == 2:
+		input_dir = Input.get_vector("izquierda2", "derecha2", "arriba2", "abajo2")
+	velocity = input_dir * SPEED
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-
+	
 	get_input()
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 	
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("izquierda", "derecha")
-	
-	if Input.is_action_pressed("arriba"):
-		velocity.y = JUMP_VELOCITY
-	if Input.is_action_pressed("abajo"):
-		velocity.y = -JUMP_VELOCITY
+	#var direction := Input.get_axis("izquierda", "derecha")
+	var direction := velocity.x
+
 
 	# Check for teleportation input
 	if direction > 0:
@@ -46,11 +43,7 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("teleportacion") and (direction < 0 or direction > 0):
 		animated_sprite.play("destello")
 	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
 	move_and_slide()
 
 
