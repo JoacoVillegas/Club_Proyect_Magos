c3021039cd31554c6fa76bd8f08d8f7ab709fba5
JoacoVillegas <cocoproman11@gmail.com>
2024-09-17 23:04:44 -0300

    Habilidad Teletransportacion

--- club_proyect/mage_1.gd
+++ club_proyect/mage_1.gd
@@ -4,7 +4,7 @@ extends CharacterBody2D
 
 @export_range(1,2) var jugador : int
 
-const SPEED = 300.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -400.0
 
 
@@ -26,27 +26,64 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	#var direction := Input.get_axis("izquierda", "derecha")
-	var direction := velocity.x
-
+	
+	var directionX := velocity.x
+	var directionY := velocity.y
 
 	# Check for teleportation input
-	if direction > 0:
+	if directionX > 0:
 		animated_sprite.flip_h = false
-	elif direction < 0:
+	elif directionX < 0:
 		animated_sprite.flip_h = true
 		
-	if direction == 0:
+	if not (directionX or directionY):
 		animated_sprite.play("quieto")
 	else:
-		animated_sprite.play("movimiento")
+		if not (is_teleporting):
+			animated_sprite.play("movimiento")
 	
-	if Input.is_action_just_pressed("teleportacion") and (direction < 0 or direction > 0):
+	if Input.is_action_just_pressed("teleportacion") and (directionX or directionY):
+		#Solucionar Bug de colision (se puede meter dentro de un muro(? xD)
 		animated_sprite.play("destello")
-	
-	
+		is_teleporting = true
+		var newPosition
+		if directionX and directionY:
+			#¿Ver los 4 casos que hay o no?
+			if directionX > 0:
+				if directionY > 0:
+					newPosition = Vector2(50 + self.global_position.x, 50 + self.global_position.y)
+				else:
+					newPosition = Vector2(50 + self.global_position.x, -50 + self.global_position.y)
+			else:
+				if directionY > 0:
+					newPosition = Vector2(-50 + self.global_position.x, 50 + self.global_position.y)
+				else:
+					newPosition = Vector2(-50 + self.global_position.x, -50 + self.global_position.y)
+		elif directionX:
+			if directionX > 0:
+				newPosition = Vector2(50 + self.global_position.x, self.global_position.y)
+			else:
+				newPosition = Vector2(-50 + self.global_position.x, self.global_position.y)
+		else:
+			if directionY > 0:
+				newPosition = Vector2(self.global_position.x, 50 + self.global_position.y)
+			else:
+				newPosition = Vector2(self.global_position.x, -50 + self.global_position.y)
+		self.set_global_position(newPosition)
 	move_and_slide()
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	 # Replace with function body.
 	print("SI LEES ESTO, ENTONCES EL MAGO SABE LEER CARTELES.")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if animated_sprite.animation == "destello":
+		
+		is_teleporting = false
+		
+		if (velocity.x != 0) or (velocity.y != 0):
+			animated_sprite.play("movimiento")
+		else:
+			animated_sprite.play("quieto")
+	
